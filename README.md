f1df0356eab9b3255dce76811e866693e65a577a
juanpabloecheverry <juanpablo.echeverry@globant.com>
2024-02-20 10:30:28 -0500

    create complaints database with complaints and users table

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 Learn FastAPI by building a complete project. Extend my knowledge on advanced web development-AWS, Payments, Emails for Udemy course
 
 ## Commands to update database
+pip install databases
+pip install alembic
+pip install databases asyncpg psycopg2-binary
+pip install asyncpg psycopg2 psycopg2-binary sqlalchemy
+alembic init migration -> This Command create the Migration folder
+
 alembic revision --autogenerate -m "Initial"
 alembic upgrade head
 
 ## using the following command to start application 
+Use pip intall uvicorn
 uvicorn main:app --reload
 
 ## swagger 
@@ -13,3 +20,9 @@ http://127.0.0.1:8000/docs
 
 ## JWT_SECRET
 It is a best practice to use UUID.uuid4 to generate the key and paste into the .env configuration file
+
+
+## Setp-up
+
+python -m venv venv
+CTRL + ALT + O -> This command order the imports
--- migrations/env.py
+++ migrations/env.py
@@ -4,12 +4,20 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from main import metadata
+from decouple import config as env_config
+from db import metadata
+import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
+section = config.config_ini_section
+config.set_section_option(section, "DB_USER", env_config("DB_USER"))
+config.set_section_option(section, "DB_PASSWORD", env_config("DB_PASSWORD"))
+config.set_section_option(section, "DB_PORT", env_config("DB_PORT"))
+config.set_section_option(section, "DB_HOST", env_config("DB_HOST"))
+config.set_section_option(section, "DB_NAME", env_config("DB_NAME"))
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
